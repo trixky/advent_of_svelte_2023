937e8507a522e506683fd1ca6729df730946f9f5
trixky <git config --global user.name>
2024-12-04 21:02:04 +0100

    first commit: start the layout

---
+++ src/lib/components/layout.ts/header.svelte
@@ -0,0 +1,26 @@
+<!-- ****************************************************** SCRIPT -->
+<script lang="ts">
+</script>
+
+<!-- ****************************************************** CONTENT -->
+<header class="flex justify-center mx-auto text-center w-full">
+  {#each new Array(2).fill(null) as _, index}
+    {@const lastLayer = index === 1}
+    <div
+      class="w-full p-8 border-b border-solid border-white bg-black bg-opacity-30"
+      class:blur-md={lastLayer}
+      class:absolute={lastLayer}
+      class:top-0={lastLayer}
+      class:opacity-50={lastLayer}
+    >
+      <h1>🎄🛷🦌 Advente of Svelte 2024 🎁⛪🧑‍🎄</h1>
+      <h2 class="text-lg text-neutral-400 font-normal italic">
+        implemented by @trixky
+      </h2>
+    </div>
+  {/each}
+</header>
+
+<!-- ****************************************************** STYLE -->
+<style lang="css">
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<!-- ****************************************************** SCRIPT -->
+<script lang="ts">
+  import "../style/reset.css";
+  import "../style/tailwind.css";
+  import "../style/main.css";
+
+  import Snow from "$lib/components/layout.ts/snow.svelte";
+  import Header from "$lib/components/layout.ts/header.svelte";
+  import Footer from "$lib/components/layout.ts/footer.svelte";
+</script>
+
+<!-- ****************************************************** CONTENT -->
+<Snow />
+<Header />
+<main class="flex flex-col gap-4 justify-center mx-auto text-center p-8">
+  <slot />
+</main>
+<Footer />
+
+<!-- ****************************************************** STYLE -->
+<style lang="css">
+</style>
