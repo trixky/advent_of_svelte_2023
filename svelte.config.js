469ac21288e77207ebc3f5ca9f83513112c836a0
trixky <git config --global user.name>
2024-12-07 12:06:32 +0100

    [Fix] action: fix github action (static)

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const baseDir = process.env.BASE_PATH || "/advent_of_svelte_2023";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +13,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: undefined,
+			precompress: false,
+			strict: true,
+		}),
+		paths: {
+			base: baseDir,
+		},
 	}
 };
 
